4bb53c162336df31d36de2fd36deab3b863445d1
santree <gosick.but@qq.com>
2018-11-21 10:17:22 +0800

    update (api): 更换api地址

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const currentEnv = process.env.NODE_ENV === 'development' ? 'rd' : 'prod'
 const origins = {
   rd: 'http://68i64h.natappfree.cc/tpxt',
-  prod: 'http://xcyzj.ngchuanmei.com/tpxt'
+  prod: 'http://xcyzj.gotoip4.com/tpxt'
 }
 const appids = {
   rd: 'wx0de73ddd52c76f3d',
